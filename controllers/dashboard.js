444a63959428b4e45f77702af770d155171b1132
Eamonn de Leastar <edeleastar@wit.ie>
2021-06-23 13:09:07 +0100

    request reading from API

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,24 +1,35 @@
 "use strict";
 
 const logger = require("../utils/logger");
-
+const axios = require("axios");
+const oneCallRequest = `https://api.openweathermap.org/data/2.5/onecall?lat=52.160858&lon=-7.152420&units=metric&appid=YOUR_API_KEY_HERE`
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
-      title: "Template 1 Dashboard",
+      title: "Template 1 Dashboard"
     };
     response.render("dashboard", viewData);
   },
   async addreport(request, response) {
     logger.info("rendering new report");
-    const report = {};
+    let report = {};
+    const result = await axios.get(oneCallRequest);
+    if (result.status == 200) {
+      const reading = result.data.current;
+      report.code = reading.weather[0].id;
+      report.temperature = reading.temp;
+      report.windSpeed = reading.wind_speed;
+      report.pressure = reading.pressure;
+      report.windDirection = reading.wind_deg;
+    }
+    console.log(report);
     const viewData = {
       title: "Weather Report",
-      reading : report
+      reading: report
     };
     response.render("dashboard", viewData);
-  },
+  }
 };
 
 module.exports = dashboard;
