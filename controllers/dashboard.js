68aa1c52a41de35d9025d4ecfa31d36b350a31e7
Eamonn de Leastar <edeleastar@wit.ie>
2021-06-23 12:41:43 +0100

    route + action (empty for the moment)

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -10,6 +10,15 @@ const dashboard = {
     };
     response.render("dashboard", viewData);
   },
+  async addreport(request, response) {
+    logger.info("rendering new report");
+    const report = {};
+    const viewData = {
+      title: "Weather Report",
+      reading : report
+    };
+    response.render("dashboard", viewData);
+  },
 };
 
 module.exports = dashboard;
